e56b4bcfbb42938f898a8277565e70d07377beba
andyvernon <andy_vernon@live.co.uk>
2022-03-21 10:28:10 +0000

    Added pages

---
+++ src/React/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello</h2>
+                <p>This is landing page paragraph 1</p>
+                <p>This is landing page paragraph 2</p>
+            </div>
+        );
+    }
+}
+
+export default Home;

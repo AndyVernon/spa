2bbbab8ddf52fde11173477d989dcd84a2170498
andyvernon <andy_vernon@live.co.uk>
2022-03-21 15:33:47 +0000

    Updated Contact structure

--- src/React/Pages/Contact/Contact.js
+++ src/React/Pages/Contact/Contact.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import BemIt from '@gtechdoodler/bem-it';
 
-class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Contact Us</h2>
-                <p>Have a look at <a href="https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm">The page this was based on</a></p>
-            </div>
-        );
-    }
+import './Contact.scss';
+
+function Contact () {
+    const bem = new BemIt('Contact');
+
+    return (
+        <div className={bem.out}>
+            <h2>Contact Us</h2>
+            <p>Have a look at <a href="https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm">The page this was based on</a></p>
+        </div>
+    );
 }
 
 export default Contact;
